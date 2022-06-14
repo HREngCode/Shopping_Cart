31bc66877bafea651939296786f8d16de0fb402c
HREngCode <ccrsailors@gmail.com>
2022-06-14 10:02:21 -0500

    ReAssigned how Product is being called.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from customer import Customer
+from product import Product
+from shopping_cart import ShoppingCart
 
 print('Please type the customer name')
 customer_one_info = Customer(input())
-
-from shopping_cart import ShoppingCart
-
 
 order_one = ShoppingCart()
 order_one.add_product_to_cart()
@@ -21,7 +20,8 @@ order_three.add_product_to_cart()
 item_cost_three = int(order_three.price_in_shopping_cart)
 print(item_cost_three)
 
-
+cart = ShoppingCart()
+cart.add_product_to_cart(Product)
 
 total_cost = (item_cost_one) + (item_cost_two) + (item_cost_three)
 print(f'Your total cost is {total_cost}')
@@ -30,9 +30,5 @@ print(f'Your total cost is {total_cost}')
 # total_cost = price_total.price_in_shopping_cart
 # price_total.calculate_total()
 # print(total_cost)
-
-
-
-
 
 # print(f'The list consists of {add_new_order.in_shopping_cart}')
